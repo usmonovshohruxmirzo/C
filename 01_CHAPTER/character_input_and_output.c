9edcbde54a0c7128d45433be06cd9e82d36d52d4
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-02 20:28:19 +0500

    feat(01_CH): add word counter func to char I/O

--- 01_CHAPTER/character_input_and_output.c
+++ 01_CHAPTER/character_input_and_output.c
@@ -1,6 +1,7 @@
-#include "inttypes.h"
-#include <math.h>
 #include <stdio.h>
+
+#define IN 1
+#define OUT 0
 
 void _EOF() { printf("%d", EOF); }
 
@@ -31,6 +32,25 @@ void lineCounter() {
   printf("%d\n lines", nl);
 }
 
+void wordCounter() {
+  int c, nl, nw, nc, state;
+
+  state = OUT;
+
+  nl = nw = nc = 0;
+  nl = (nw = (nc = 0));
+
+  while ((c = getchar()) != EOF) {
+    ++nc;
+    if (c == ' ' || c == '\n' || c == '\t') {
+      state = OUT;
+    } else if (state == IN) {
+      ++nw;
+    }
+  }
+  printf("%d %d %d\n", nl, nw, nc);
+}
+
 int main() {
   int c;
 
@@ -44,7 +64,10 @@ int main() {
   // }
 
   // charCounter();
-  lineCounter();
+
+  // lineCounter();
+
+  wordCounter();
 
   return 0;
 }
