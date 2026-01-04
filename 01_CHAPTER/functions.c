96cbedb86d41a36a7ccd34501c517ae8f8a8b518
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-04 18:39:19 +0500

    feat(01_CH, functions): update functions

--- 01_CHAPTER/functions.c
+++ 01_CHAPTER/functions.c
@@ -6,20 +6,43 @@ int main() {
   int i;
 
   for (i = 0; i < 10; ++i) {
-    printf("%d %5d %10d\n", i, power(2, i), power(-3, i));
+    printf("%d) %5d %10d\n", i, power(2, i), power(-3, i));
   }
 
   return 0;
 }
 
+// int power(int base, int n) {
+//   int i, p;
+//
+//   p = 1;
+//
+//   for (i = 1; i <= n; ++i) {
+//     p = p * base;
+//   }
+//
+//   return p;
+// }
+
+/* power:  raise base to n-th power; n >= 0 */
+/*         (old-style version) */
+// power2(base, n)
+// int base, n;
+// {
+//     int i, p;
+//     p = 1;
+//     for (i = 1; i <= n; ++i)
+//         p = p * base;
+//     return p;
+// }
+
+
+
+// 1.8 Arguments - Call by Value
 int power(int base, int n) {
-  int i, p;
-
-  p = 1;
-
-  for (i = 1; i <= n; ++i) {
+  int p;
+  for (p = 1; n > 0; --n)
     p = p * base;
-  }
-
   return p;
 }
+
